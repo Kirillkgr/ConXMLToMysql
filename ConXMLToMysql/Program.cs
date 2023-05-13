f116b1c202c213a36acc3e290999e61f56b36ac5
rill <RillGd@gmail.com>
2023-05-13 19:57:08 +0200

    Добавил аргументу в программу. Шаблон ({pathToClientFile} -client {pathToCardFile} -card {ipAddressToDB} -server  {nameDB} -database {userName} -username {pass} -pass) порядок заполнение шаблонов не имеит значени. Если один из 4 параметров для подключение к базе не будит указан будут применины настройки по умолчанию.

--- ConXMLToMysql/Program.cs
+++ ConXMLToMysql/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using ConXMLToMysql.ConectDB;
+
 namespace ConXMLToMysql
 {
     internal abstract class Program
     {
         public static void Main(string[] args)
         {
-            string[] connect = { "127.0.0.1", "testdb", "root", "123456;" };
-            const string pathCardsFile = ("/home/rill/Documents/Cards_20211005080948.xml");
-            const string pathClientsFile = ("/home/rill/Documents/Clients.xml");
-            
+            var pathCardsFile = "";
+            var pathClientsFile = "";
+            var server = "";
+            var database = "";
+            var username = "";
+            var pass = "";
+            // var pathCardsFile = ("/home/rill/Documents/Cards_20211005080948.xml");
+            // var pathClientsFile = ("/home/rill/Documents/Clients.xml");
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-card")
+                {
+                    pathCardsFile = args[i - 1];
+                }
+
+                if (args[i] == "-client")
+                {
+                    pathClientsFile = args[i - 1];
+                }
+
+                if (args[i] == "-server")
+                {
+                    server = args[i - 1];
+                }
+
+                if (args[i] == "-database")
+                {
+                    database = args[i - 1];
+                }
+
+                if (args[i] == "-pass")
+                {
+                    pass = args[i - 1];
+                }
+
+                if (args[i] == "-username")
+                {
+                    username = args[i - 1];
+                }
+            }
+
+            // /home/rill/Documents/Clients.xml -client /home/rill/Documents/Cards_20211005080948.xml -card 127.0.0.1 -server  testdb -database root -username 123456 -pass
+            // {pathToClientFile} -client {pathToCardFile} -card {ipAddressToDB} -server  {nameDB} -database {userName} -username {pass} -pass
+
+            if (pathClientsFile.Length < 1)
+                pathClientsFile = "/home/rill/Documents/Clients.xml";
+            if (pathCardsFile.Length < 1)
+                pathCardsFile = "/home/rill/Documents/Cards_20211005080948.xml";
+            string[] connect ={ "127.0.0.1", "testdb", "root", "123456;" };
+            if (server.Length > 7 && database.Length > 2 && pass.Length > 4 && username.Length > 3)
+            {
+                connect= new[] { server, database, username, pass };
+            }
+
 
             Console.WriteLine(new RecordingCard(connect).StartParseCardToDb(pathCardsFile)
                 ? "Card success write to DB"
